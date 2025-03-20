5753f0f74ee60c9ffb5953358eb262cfb2062362
pdk1399 <khoa199960@gmail.com>
2025-03-20 18:20:04 +0700

    Update StickTowerField.ts

--- playable-3.8.3/assets/Script/else/stick-tower/StickTowerField.ts
+++ playable-3.8.3/assets/Script/else/stick-tower/StickTowerField.ts
@@ -3,12 +3,6 @@ const { ccclass, property } = _decorator;
 
 @ccclass('StickTowerManager')
 export class StickTowerManager extends Component {
-    start() {
 
-    }
 
-    update(deltaTime: number) {
-        
-    }
 }
-
