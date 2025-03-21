ba27dceea33a44f95a3c08a5c8a334f51af19a9e
pdk1399 <khoa199960@gmail.com>
2025-03-21 11:52:56 +0700

    update stick tower player & enermy in room

--- playable-3.8.3/assets/Script/else/stick-tower/StickTowerRoom.ts
+++ playable-3.8.3/assets/Script/else/stick-tower/StickTowerRoom.ts
@@ -6,6 +6,7 @@ const { ccclass, property } = _decorator;
 @ccclass('StickTowerRoom')
 export class StickTowerRoom extends Component {
 
+    m_player: StickTowerUnit = null;
     m_unit: StickTowerUnit[] = [];
 
     protected onLoad(): void {
@@ -16,30 +17,42 @@ export class StickTowerRoom extends Component {
     protected start(): void {
         this.node.children.forEach(child => {
             let unit = child.getComponent(StickTowerUnit);
-            if (unit != null ? !unit.Player : false)
-                this.m_unit.push(unit);
+            if (unit != null) {
+                if (unit.Player)
+                    this.m_player = unit;
+                else
+                    this.m_unit.push(unit);
+            }
         });
     }
 
     onPlayerEnter(target: Node) {
         let unit = target.getComponent(StickTowerUnit);
-        if (unit == null ? true : !unit.Player)
+        if (unit == null)
             return;
-
-        //Player start attack enermy in room
-        this.scheduleOnce(() => {
-            if (this.m_unit.length > 0) {
+        if (!unit.Player)
+            return;
+        this.m_player = unit;
+        if (this.m_unit.length > 0) {
+            //Player Attack
+            this.scheduleOnce(() => {
+                this.m_player.onUnitIdle();
+            }, this.m_player.onUnitAttack());
+            //Enermy Dead
+            this.scheduleOnce(() => {
                 this.m_unit[this.m_unit.length - 1].node.destroy();
                 this.m_unit.splice(this.m_unit.length - 1, 1);
-            }
-        });
+            }, this.m_unit[this.m_unit.length - 1].onUnitDead());
+        }
     }
 
     onPlayerExit(target: Node) {
         let unit = target.getComponent(StickTowerUnit);
-        if (unit == null ? true : !unit.Player)
+        if (unit == null)
             return;
-
+        if (!unit.Player)
+            return;
+        this.m_player = null;
         //Destroy tower if no unit left
     }
 }
--- playable-3.8.3/assets/Script/else/stick-tower/StickTowerUnit.ts
+++ playable-3.8.3/assets/Script/else/stick-tower/StickTowerUnit.ts
@@ -1,4 +1,5 @@
-import { _decorator, CCBoolean, Component, Node } from 'cc';
+import { _decorator, CCBoolean, CCString, Component, Node } from 'cc';
+import { SpineBase } from '../../renderer/SpineBase';
 const { ccclass, property } = _decorator;
 
 @ccclass('StickTowerUnit')
@@ -6,4 +7,29 @@ export class StickTowerUnit extends Component {
 
     @property({ group: { name: 'Main' }, type: CCBoolean })
     Player: boolean = false;
+
+    @property({ group: { name: 'Anim' }, type: CCString })
+    AnimIdle: string = 'idle';
+    @property({ group: { name: 'Anim' }, type: CCString })
+    AnimAttack: string = 'attack';
+    @property({ group: { name: 'Anim' }, type: CCString })
+    AnimDead: string = 'dead';
+
+    m_spine: SpineBase = null;
+
+    protected onLoad(): void {
+        this.m_spine = this.getComponent(SpineBase);
+    }
+
+    onUnitIdle(): number {
+        return this.m_spine.onAnimation(this.AnimIdle, true);
+    }
+
+    onUnitAttack(): number {
+        return this.m_spine.onAnimation(this.AnimAttack, false);
+    }
+
+    onUnitDead(): number {
+        return this.m_spine.onAnimation(this.AnimDead, false);
+    }
 }
