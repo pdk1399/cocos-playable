623379716422bb867e78a23f87e53fc6ef5aaa66
pdk1399 <khoa199960@gmail.com>
2025-05-14 18:41:57 +0700

    update emit control

--- playable-3.8.3/assets/Script/emit/EmitControl.ts
+++ playable-3.8.3/assets/Script/emit/EmitControl.ts
@@ -1,38 +1,55 @@
-import { _decorator, CCBoolean, director, Node } from 'cc';
+import { _decorator, CCBoolean, director, Enum, Node } from 'cc';
 import { ConstantBase } from '../ConstantBase';
 import { EmitBaseFull } from './EmitBaseFull';
 const { ccclass, property } = _decorator;
 
+export enum ControlType {
+    None = 0,
+    Director = 1,
+    Node = 2,
+}
+Enum(ControlType);
+
+export enum BodyType {
+    Sleep,
+    Awake,
+}
+Enum(BodyType);
+
+export enum ReleaseType {
+    None = 0,
+    X = 1,
+    Y = 2,
+    XY = 3,
+}
+Enum(ReleaseType);
+
 @ccclass('EmitControl')
 export class EmitControl extends EmitBaseFull {
 
-    @property({ group: { name: 'Main' }, type: CCBoolean })
-    Control: boolean = true;
-    @property({ group: { name: 'Main' }, type: CCBoolean, visible(this: EmitControl) { return this.Control; } })
-    ControlByNode: boolean = true;
-    @property({ group: { name: 'Main' }, type: CCBoolean, visible(this: EmitControl) { return this.Control && this.ControlByNode; } })
-    ControlSleep: boolean = false;
-    @property({ group: { name: 'Main' }, type: CCBoolean, visible(this: EmitControl) { return this.Control && this.ControlByNode && !this.ControlSleep; } })
-    ControlRelease: boolean = false;
-    @property({ group: { name: 'Main' }, type: CCBoolean, visible(this: EmitControl) { return this.Control && this.ControlByNode && !this.ControlSleep && !this.ControlRelease; } })
-    ControlReleaseX: boolean = false;
-    @property({ group: { name: 'Main' }, type: CCBoolean, visible(this: EmitControl) { return this.Control && this.ControlByNode && !this.ControlSleep && !this.ControlRelease; } })
-    ControlReleaseY: boolean = false;
-    @property({ group: { name: 'Main' }, type: CCBoolean, visible(this: EmitControl) { return this.Control && this.ControlByNode && !this.ControlSleep && !this.ControlRelease && !this.ControlReleaseX; } })
+    @property({ group: { name: 'Main' }, type: ControlType })
+    Control: ControlType = ControlType.Node;
+    @property({ group: { name: 'Main' }, type: BodyType, visible(this: EmitControl) { return this.Control >= ControlType.Node; } })
+    ControlBody: BodyType = BodyType.Awake;
+    @property({ group: { name: 'Main' }, type: ReleaseType, visible(this: EmitControl) { return this.Control >= ControlType.Node && this.ControlBody >= BodyType.Awake; } })
+    ControlRelease: ReleaseType = ReleaseType.None;
+
+    @property({ group: { name: 'Main' }, type: CCBoolean, visible(this: EmitControl) { return this.Control >= ControlType.Node && this.ControlBody >= BodyType.Awake && this.ControlRelease != ReleaseType.XY && this.ControlRelease != ReleaseType.X; } })
     ControlLeft: boolean = false;
-    @property({ group: { name: 'Main' }, type: CCBoolean, visible(this: EmitControl) { return this.Control && this.ControlByNode && !this.ControlSleep && !this.ControlRelease && !this.ControlReleaseX; } })
+    @property({ group: { name: 'Main' }, type: CCBoolean, visible(this: EmitControl) { return this.Control >= ControlType.Node && this.ControlBody >= BodyType.Awake && this.ControlRelease != ReleaseType.XY && this.ControlRelease != ReleaseType.X; } })
     ControlRight: boolean = false;
-    @property({ group: { name: 'Main' }, type: CCBoolean, visible(this: EmitControl) { return this.Control && this.ControlByNode && !this.ControlSleep && !this.ControlRelease && !this.ControlReleaseY; } })
+    @property({ group: { name: 'Main' }, type: CCBoolean, visible(this: EmitControl) { return this.Control >= ControlType.Node && this.ControlBody >= BodyType.Awake && this.ControlRelease != ReleaseType.XY && this.ControlRelease != ReleaseType.Y; } })
     ControlUp: boolean = false;
-    @property({ group: { name: 'Main' }, type: CCBoolean, visible(this: EmitControl) { return this.Control && this.ControlByNode && !this.ControlSleep && !this.ControlRelease && !this.ControlReleaseY; } })
+    @property({ group: { name: 'Main' }, type: CCBoolean, visible(this: EmitControl) { return this.Control >= ControlType.Node && this.ControlBody >= BodyType.Awake && this.ControlRelease != ReleaseType.XY && this.ControlRelease != ReleaseType.Y; } })
     ControlDown: boolean = false;
-    @property({ group: { name: 'Main' }, type: CCBoolean, visible(this: EmitControl) { return this.Control && this.ControlByNode && !this.ControlSleep && !this.ControlRelease && !this.ControlReleaseY; } })
+    @property({ group: { name: 'Main' }, type: CCBoolean, visible(this: EmitControl) { return this.Control >= ControlType.Node && this.ControlBody >= BodyType.Awake && this.ControlRelease != ReleaseType.XY && this.ControlRelease != ReleaseType.Y; } })
     ControlJump: boolean = false;
-    @property({ group: { name: 'Main' }, type: CCBoolean, visible(this: EmitControl) { return this.Control && this.ControlByNode; } })
+
+    @property({ group: { name: 'Option' }, type: CCBoolean, visible(this: EmitControl) { return this.Control >= ControlType.Node; } })
     ControlAttack: boolean = false;
-    @property({ group: { name: 'Main' }, type: CCBoolean, visible(this: EmitControl) { return this.Control && this.ControlByNode; } })
+    @property({ group: { name: 'Option' }, type: CCBoolean, visible(this: EmitControl) { return this.Control >= ControlType.Node; } })
     ControlInteraction: boolean = false;
-    @property({ group: { name: 'Main' }, type: CCBoolean, visible(this: EmitControl) { return this.Control && this.ControlByNode && !this.ControlSleep; } })
+    @property({ group: { name: 'Option' }, type: CCBoolean, visible(this: EmitControl) { return this.Control >= ControlType.Node && this.ControlBody >= BodyType.Awake; } })
     ControlFixed: boolean = false;
 
     @property({ group: { name: 'Option' }, type: CCBoolean })
@@ -42,73 +59,69 @@ export class EmitControl extends EmitBaseFull {
 
     onEventActiveNode(target: Node): void {
         //OPTION
-        if (this.BodyX2) {
-            if (this.ControlByNode)
-                target.emit(ConstantBase.NODE_BODY_X2);
-            else
-                director.emit(ConstantBase.NODE_BODY_X2);
-        }
-
-        if (this.BodyX4) {
-            if (this.ControlByNode)
-                target.emit(ConstantBase.NODE_BODY_X4);
-            else
-                director.emit(ConstantBase.NODE_BODY_X4);
-        }
-
-        if (!this.Control)
-            return;
-        //MAIN
-        if (this.ControlByNode) {
-            director.emit(ConstantBase.CONTROL_LOCK);
-            target.emit(ConstantBase.NODE_CONTROL_NODE, true);
-            target.emit(ConstantBase.NODE_CONTROL_DIRECTOR, false);
-        }
-        else {
-            director.emit(ConstantBase.CONTROL_RESUME);
-            target.emit(ConstantBase.NODE_CONTROL_NODE, false);
-            target.emit(ConstantBase.NODE_CONTROL_DIRECTOR, true);
-            return;
-        }
-
-        if (this.ControlSleep) {
-            target.emit(ConstantBase.NODE_CONTROL_SLEEP);
-            return;
-        }
-        else
-            target.emit(ConstantBase.NODE_CONTROL_AWAKE);
-
-        if (this.ControlRelease)
-            target.emit(ConstantBase.CONTROL_RELEASE);
-        else {
-            if (this.ControlReleaseX)
-                target.emit(ConstantBase.CONTROL_RELEASE_X);
-            else {
-                if (this.ControlLeft)
-                    target.emit(ConstantBase.CONTROL_LEFT);
-                if (this.ControlRight)
-                    target.emit(ConstantBase.CONTROL_RIGHT);
-            }
-
-            if (this.ControlReleaseY)
-                target.emit(ConstantBase.CONTROL_RELEASE_Y);
-            else {
-                if (this.ControlUp)
-                    target.emit(ConstantBase.CONTROL_UP);
-                if (this.ControlDown)
-                    target.emit(ConstantBase.CONTROL_DOWN);
-                if (this.ControlJump)
-                    target.emit(ConstantBase.CONTROL_JUMP);
-            }
-        }
+        if (this.BodyX2)
+            target.emit(ConstantBase.NODE_BODY_X2);
+        if (this.BodyX4)
+            target.emit(ConstantBase.NODE_BODY_X4);
 
         if (this.ControlAttack)
             target.emit(ConstantBase.CONTROL_ATTACK);
-
         if (this.ControlInteraction)
             target.emit(ConstantBase.CONTROL_INTERACTION);
-
         if (this.ControlFixed)
             target.emit(ConstantBase.CONTROL_FIXED);
+
+        //MAIN
+        switch (this.Control) {
+            case ControlType.None:
+                return;
+            case ControlType.Director:
+                director.emit(ConstantBase.CONTROL_RESUME);
+                target.emit(ConstantBase.NODE_CONTROL_NODE, false);
+                target.emit(ConstantBase.NODE_CONTROL_DIRECTOR, true);
+                return;
+            case ControlType.Node:
+                director.emit(ConstantBase.CONTROL_LOCK);
+                target.emit(ConstantBase.NODE_CONTROL_NODE, true);
+                target.emit(ConstantBase.NODE_CONTROL_DIRECTOR, false);
+                break;
+        }
+        switch (this.ControlBody) {
+            case BodyType.Awake:
+                target.emit(ConstantBase.NODE_CONTROL_AWAKE);
+                break;
+            case BodyType.Sleep:
+                target.emit(ConstantBase.NODE_CONTROL_SLEEP);
+                return;
+        }
+        switch (this.ControlRelease) {
+            case ReleaseType.None:
+                break;
+            case ReleaseType.X:
+                target.emit(ConstantBase.CONTROL_RELEASE_X);
+                break;
+            case ReleaseType.Y:
+                target.emit(ConstantBase.CONTROL_RELEASE_Y);
+                break;
+            case ReleaseType.XY:
+                target.emit(ConstantBase.CONTROL_RELEASE);
+                return;
+        }
+
+        if (this.ControlRelease != ReleaseType.X) {
+            if (this.ControlLeft)
+                target.emit(ConstantBase.CONTROL_LEFT);
+            if (this.ControlRight)
+                target.emit(ConstantBase.CONTROL_RIGHT);
+        }
+
+        if (this.ControlRelease != ReleaseType.Y) {
+            if (this.ControlUp)
+                target.emit(ConstantBase.CONTROL_UP);
+            if (this.ControlDown)
+                target.emit(ConstantBase.CONTROL_DOWN);
+            if (this.ControlJump)
+                target.emit(ConstantBase.CONTROL_JUMP);
+        }
     }
 }
