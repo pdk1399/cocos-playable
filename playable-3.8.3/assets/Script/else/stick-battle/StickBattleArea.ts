992184297a7ba9053fd5da4e2212006a597a4fc1
pdk1399 <khoa199960@gmail.com>
2025-08-01 18:04:03 +0700

    update

--- playable-3.8.3/assets/Script/else/stick-battle/StickBattleArea.ts
+++ playable-3.8.3/assets/Script/else/stick-battle/StickBattleArea.ts
@@ -1,5 +1,8 @@
 import { _decorator, Component, Node } from 'cc';
 import { ConstantBase } from '../../ConstantBase';
+import { UIDrop } from '../../ui/drag-drop/UIDrop';
+import { UIDrag } from '../../ui/drag-drop/UIDrag';
+import { StickBattleUnit } from './StickBattleUnit';
 const { ccclass, property } = _decorator;
 
 @ccclass('StickBattleArea')
@@ -8,16 +11,60 @@ export class StickBattleArea extends Component {
     @property(Node)
     mask: Node = null;
 
+    m_uiDrop: UIDrop = null;
+
     protected onLoad(): void {
-        this.node.on(ConstantBase.NODE_UI_DRAG_ENTER, this.onDragEnter, this);
-        this.node.on(ConstantBase.NODE_UI_DRAG_EXIT, this.onDragExit, this);
+        this.m_uiDrop = this.getComponent(UIDrop);
+
+        this.node.on(ConstantBase.NODE_UI_DRAG_ENTER, this.onMaskShow, this);
+        this.node.on(ConstantBase.NODE_UI_DRAG_EXIT, this.onMaskHide, this);
+
+        this.node.on(ConstantBase.NODE_UI_DROP_ENTER, this.onUnitMerge, this);
     }
 
-    onDragEnter() {
+    //DRAG
+
+    onMaskShow() {
         this.mask.active = true;
     }
 
-    onDragExit() {
+    onMaskHide() {
         this.mask.active = false;
+    }
+
+    //UNIT
+
+    onUnitMerge() {
+        if (this.m_uiDrop.m_uiDrag.length != 2) {
+            console.log('Cannot merge, need exactly 2 units (current: ' + this.m_uiDrop.m_uiDrag.length + ')');
+            return;
+        }
+        let unitA = this.m_uiDrop.m_uiDrag[0].getComponent(StickBattleUnit);
+        let unitB = this.m_uiDrop.m_uiDrag[1].getComponent(StickBattleUnit);
+        if (unitA == null || unitB == null) {
+            console.log('Cannot merge, unit not found component');
+            return;
+        }
+        if (unitA.type != unitB.type || unitA.level != unitB.level) {
+            console.log('Cannot merge, not same type and level');
+            this.onUnitSwap();
+        }
+        else {
+            if (!unitA.onLevelAdd()) {
+                console.log('Cannot merge, unit level is max');
+                this.onUnitSwap();
+                return;
+            }
+            console.log('Merge unit complete');
+            unitB.getComponent(UIDrag).onDropExit();
+            this.scheduleOnce(() => unitB.node.destroy(), 0.02);
+        }
+    }
+
+    onUnitSwap() {
+        console.log('Swap units');
+        let dragA = this.m_uiDrop.m_uiDrag[0].getComponent(UIDrag);
+        let dragB = this.m_uiDrop.m_uiDrag[1].getComponent(UIDrag);
+        dragA.onDropEnter(dragB.m_uiDropLast);
     }
 }
