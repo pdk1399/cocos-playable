c39d70a1b44a3ce5f9438c83912376f3abe899bb
pdk1399 <khoa199960@gmail.com>
2025-08-12 14:23:40 +0700

    Update UIDragSingle.ts

--- playable-3.8.6/assets/Script/ui/drag-drop/UIDragSingle.ts
+++ playable-3.8.6/assets/Script/ui/drag-drop/UIDragSingle.ts
@@ -9,19 +9,18 @@ export class UIDragSingle extends Component {
     //- This will check combine bound of child's UI-Transform component both, not only own UI-Transform
     //- This might work best for Anchor Point Centre (0.5; 0.5), mean Sprite than Spine (Skeleton)
 
-    @property({ type: [Node], group: { name: 'Main' } })
-    GroupDrop: Node[] = [];
-
-    @property({ type: CCBoolean, group: { name: 'Drop' } })
+    @property({ type: [Node] })
+    DropGroup: Node[] = [];
+    @property({ type: CCBoolean })
     DropPosReset: boolean = false;
-    @property({ type: CCBoolean, group: { name: 'Drop' } })
+    @property({ type: CCBoolean })
     DropPosFixed: boolean = false;
-    @property({ type: CCBoolean, group: { name: 'Drop' } })
+    @property({ type: CCBoolean })
     DropPosUpdate: boolean = false;
 
     m_pos: Vec3 = new Vec3();
     m_drag: boolean = false;
-    m_drop: Node = null;
+    m_contact: Node = null;
 
     protected onLoad(): void {
         this.node.on(Node.EventType.TOUCH_START, this.onTouchStart, this);
@@ -31,9 +30,10 @@ export class UIDragSingle extends Component {
     }
 
     protected start(): void {
-        if (this.GroupDrop.length == 0)
-            this.GroupDrop.push(this.node.parent);
+        if (this.DropGroup.length == 0)
+            this.DropGroup.push(this.node.parent);
         this.m_pos = this.node.getPosition().clone();
+        this.m_contact = this.getContactClose();
     }
 
     protected onDestroy(): void {
@@ -46,6 +46,7 @@ export class UIDragSingle extends Component {
     onTouchStart(event: EventTouch) {
         this.m_drag = true;
         this.node.setSiblingIndex(999);
+        this.m_contact?.emit(ConstantBase.NODE_UI_DROP_EXIT, this.node);
         this.node.emit(ConstantBase.NODE_UI_DRAG_START, this.node);
     }
 
@@ -62,16 +63,21 @@ export class UIDragSingle extends Component {
             return;
         this.m_drag = false;
 
-        const nodeContact = this.getContactClose();
-        if (nodeContact != null) {
+        this.m_contact?.emit(ConstantBase.NODE_UI_DROP_EXIT, this.node);
+        this.m_contact = this.getContactClose();
+        if (this.m_contact != null) {
             if (this.DropPosFixed)
-                this.node.setWorldPosition(nodeContact.getWorldPosition().clone());
+                this.node.setWorldPosition(this.m_contact.getWorldPosition().clone());
             if (this.DropPosUpdate)
                 this.m_pos = this.node.getPosition().clone();
+
+            this.m_contact?.emit(ConstantBase.NODE_UI_DROP_ENTER, this.node);
         }
         else {
             if (this.DropPosReset)
                 this.node.setPosition(this.m_pos);
+
+            this.m_contact?.emit(ConstantBase.NODE_UI_DROP_BACK, this.node);
         }
 
         this.node.emit(ConstantBase.NODE_UI_DRAG_END, this.node);
@@ -99,7 +105,7 @@ export class UIDragSingle extends Component {
             return [];
         const fromDragBound = fromDragUI.getBoundingBoxToWorld();
         const result: Node[] = [];
-        for (const groupDrop of this.GroupDrop) {
+        for (const groupDrop of this.DropGroup) {
             for (const fromDrop of groupDrop.children) {
                 if (fromDrop === this.node || !fromDrop.active)
                     continue;
