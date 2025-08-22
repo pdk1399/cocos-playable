6f9e377a5b0f26aebe11d75b8c45790998341b5a
pdk1399 <khoa199960@gmail.com>
2025-08-22 11:55:29 +0700

    update player loop base

---
+++ playable-3.8.6/assets/Script/else/idle-quest/IdleQuestPlayerLoop.ts
@@ -0,0 +1,30 @@
+import { _decorator, CCFloat, Component, director, Node } from 'cc';
+import { ConstantBase } from '../../ConstantBase';
+import { BodyBase } from '../../body/BodyBase';
+const { ccclass, property } = _decorator;
+
+@ccclass('IdleQuestPlayerLoop')
+export class IdleQuestPlayerLoop extends Component {
+
+    @property({ type: Node })
+    BodyPlayer: BodyBase = null;
+
+    @property({ type: Node })
+    EventAttackLoop: Node = null;
+
+    @property({ type: CCFloat })
+    EventDelayLoop: number = 1;
+
+    protected onLoad(): void {
+        this.node.on(ConstantBase.NODE_EVENT, this.onPlayerLoop, this);
+    }
+
+    private onPlayerLoop(): void {
+        if (this.BodyPlayer.m_dead) {
+            director.emit(ConstantBase.GAME_LOSE);
+            return;
+        }
+        this.EventAttackLoop.emit(ConstantBase.NODE_EVENT);
+        this.scheduleOnce(() => this.onPlayerLoop(), this.EventDelayLoop);
+    }
+}
